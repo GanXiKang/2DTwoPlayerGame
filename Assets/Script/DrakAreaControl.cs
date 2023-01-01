28fff885ef0246bc8e64a214a44287823f4ec5e3
GanXiKang <gan0612xk@hotmail.com>
2023-01-02 00:07:34 +0800

    feat ： 添加暗領域

--- Assets/Script/DrakAreaControl.cs
+++ Assets/Script/DrakAreaControl.cs
@@ -2,11 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LightAreaControl : MonoBehaviour
-{ 
+public class DrakAreaControl : MonoBehaviour
+{
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.name == "Player2")
+        if (collision.gameObject.name == "Player1")
         {
             collision.gameObject.SetActive(false);
             Destroy(collision.gameObject);
