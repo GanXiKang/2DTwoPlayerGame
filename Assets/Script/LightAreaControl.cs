f130301147798b8b5ae3f84d672c10cfb1f37ef5
GanXiKang <gan0612xk@hotmail.com>
2023-01-01 23:51:45 +0800

    feat : 添加光領域

--- Assets/Script/LightAreaControl.cs
+++ Assets/Script/LightAreaControl.cs
@@ -2,22 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CoinControl : MonoBehaviour
-{
+public class LightAreaControl : MonoBehaviour
+{ 
     void Start()
     {
         
     }
     void Update()
     {
-        transform.Rotate(0, 45f * Time.deltaTime, 0);
+        
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.tag == "Player")
+        if (collision.gameObject.name == "Player2")
         {
-            gameObject.SetActive(false);
-            Destroy(gameObject);
+            collision.gameObject.SetActive(false);
+            Destroy(collision.gameObject);
         }
     }
 }
