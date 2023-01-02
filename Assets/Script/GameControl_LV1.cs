64c71e9a3ef8a299cc27e226ee3cca0b3dd17f31
GanXiKang <gan0612xk@hotmail.com>
2023-01-02 23:21:03 +0800

    feat ： 建立Menu關卡&LV1GameObject

---
+++ Assets/Script/GameControl_LV1.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameControl_LV1 : MonoBehaviour
+{ 
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
