a9a39c282f5e888dd446ef8232eec975710c273c
GanXiKang <gan0612xk@hotmail.com>
2023-01-07 22:11:49 +0800

    feat ： 添加了角色Jump的音效（質感奇怪）

--- Assets/Script/Player1Control.cs
+++ Assets/Script/Player1Control.cs
@@ -4,12 +4,12 @@ using UnityEngine;
 
 public class Player1Control : MonoBehaviour
 {
+    public AudioSource bGM;
+    public AudioClip j;
+
     private float speed = 5f;
     private float jump = 8f ;
-    void Start()
-    {
-        
-    }
+
     void Update()
     {
         if (Input.GetKey("a"))
@@ -22,6 +22,7 @@ public class Player1Control : MonoBehaviour
         }
         if (Input.GetKey("w"))
         {
+            bGM.PlayOneShot(j);
             transform.Translate(0, jump * Time.deltaTime, 0);
         }
     }
